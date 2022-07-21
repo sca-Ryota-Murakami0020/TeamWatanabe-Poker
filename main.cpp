8dac4822ec295cdaee67a715a14f7d3c24983b10
sca-Ryota-Murakami0020 <21cge0020mr@edu.sca.ac.jp>
2022-07-21 19:50:03 +0900

    main.cppの開始

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+    int card[5][4][13];
+    int number;
+    int job;
+    int power;
+
+    number=1;
+    job=1;
+    power=1;
+    for(int i=1;i<=5;i++)
+    {
+        card[i][1][1];
+    }
+
+    for(number=1;number<=6;number++)
+    [
+        
+    ]
+}
